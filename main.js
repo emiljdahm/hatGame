2e56b37970f29657b089a2a8f45a4407e36fa384
emiljdahm <emiljdahm@gmail.com>
2024-11-16 21:50:19 -0800

    redone sections of field,got goal and start avaliable, got string code set, need to figure out how I set multiple objects

--- main.js
+++ main.js
@@ -17,21 +17,40 @@ function generateField(rows,cols){
   //for loop to create the field
 for(let i = 0; i < rows; i++){
 
-  //CREATE NEW ROW 
+  //CREATE NEW ROW 5 rows
   let row = [];
 
   for(let j = 0; j < cols; j++){
-    //pushes value into ea row[j]
+    //pushes value into ea row[j] 5 values in ea row
     row.push(fieldCharacter);
   }
   //push ea row into array 
  gameField.push(row)
 }
 
-//array to string
-let stringField = gameField.join('').join
-console.log(stringField)
+const objRowSelc = Math.floor(Math.random()*(gameField.length))
+const objcColSelc = Math.floor(Math.random()*(gameField.length))
+
+//random set goal
+const goalrowSelc = Math.floor(Math.random()*(gameField.length))
+const goalcolSelc = Math.floor(Math.random()*(gameField.length))
+//inset validation to remove 
+
+console.log(goalrowSelc)
+console.log(goalcolSelc)
+  gameField[goalrowSelc][goalcolSelc] = hat
+
+  //set start 
+  gameField[0][0] = pathCharacter
+
+
+
+
+
+
+let stringField = gameField.join('').replace(/,/g, '')
+console.log(gameField)
 }
 
-console.log(generateField(5,5))
-console.log(gameField[0][0])
+
+generateField(5,5)
